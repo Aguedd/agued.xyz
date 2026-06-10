e24f811fe9fb6d1061c65f441d71880362de8147
Agued <143961080+Aguedd@users.noreply.github.com>
2026-06-10 16:07:40 +0200

    v2.1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function devToolMessage() {
   console.log("Welcome to my JS, brave traveler.");
   console.log("Don't ask.");
-  console.log("this is cool, I guess...");
+  console.log("this is cool, I guess..");
 }
 
 devToolMessage();
